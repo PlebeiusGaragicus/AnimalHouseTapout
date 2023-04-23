d7c4d781a03d91285ff98bd36a366e635440df35
plebg <plebeiusgaragicus@gmail.com>
2023-04-22 20:33:15 -0700

    loggly

--- server/logger.js
+++ server/logger.js
@@ -3,6 +3,16 @@
 import winston from 'winston';
 import moment from 'moment';
 import 'moment-timezone';
+const { Loggly } = require('winston-loggly-bulk');
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+const LOGGLY_CUSTOMER_TOKEN = process.env.LOGGLY_CUSTOMER_TOKEN;
+if (LOGGLY_CUSTOMER_TOKEN === undefined) {
+    console.log('LOGGLY_CUSTOMER_TOKEN is undefined.  Please set it in your .env file.');
+    process.exit(1);
+}
 
 
 const consoleFormat = winston.format.printf(({ timestamp, level, message, stack }) => {
@@ -39,12 +49,19 @@ const logger = winston.createLogger({
         winston.format.splat(),
         consoleFormat
     ),
+    defaultMeta: { service: 'your-service-name' },
     transports: [
         new winston.transports.Console({
             format: winston.format.combine(
                 winston.format.colorize(),
                 consoleFormat
             )
+        }),
+        new Loggly({
+            token: 'LOGGLY_CUSTOMER_TOKEN',
+            subdomain: 'AnimalHouseTap',
+            tags: ['Winston-NodeJS'],
+            json: true,
         }),
         new winston.transports.File({ filename: './logs/error.log', level: 'error', format: fileFormat }),
         new winston.transports.File({ filename: `./logs/errors-${Date.now()}.log`, level: 'error', format: fileFormat, options: { flags: 'w' } }),
